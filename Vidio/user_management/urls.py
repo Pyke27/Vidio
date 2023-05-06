a5af4f000bcbfd6936d01ff0c289f895b8686085
Pyke27 <132732093+Pyke27@users.noreply.github.com>
2023-05-06 05:07:24 +0000

    Update urls.py

--- Vidio/user_management/urls.py
+++ Vidio/user_management/urls.py
@@ -33,5 +33,7 @@ urlpatterns = [
     path('password-change/', ChangePasswordView.as_view(), name='password_change'),
 
     re_path(r'^oauth/', include('social_django.urls', namespace='social')),
+        url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
